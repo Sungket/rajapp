53bc3aa515b78694f927cdbce6351ae58ffa5db7
sungket <93491754+Sungket@users.noreply.github.com>
2024-12-18 15:32:14 +0000

    added Title1.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Title1 from './Title1.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+	<Title1 />
+	<Title1 />  
         <img src={logo} className="App-logo" alt="logo" />
 	<Button>Test Button</Button>
 	<div className="App">
@@ -15,14 +18,6 @@ function App() {
       </header>
     </div>
   );
-}
-
-
-function Logo() {
-  return (
-    <h1>Raj Music App</h1>
-  )
-
 }
 
 export default App;
---
+++ src/Title1.js
@@ -0,0 +1,5 @@
+export default function Title1() {
+   return (
+      <h1>Yello!</h1>
+   )
+}
