f893378b285f86505e8222a97a30e8b36b6424bb
sungket <93491754+Sungket@users.noreply.github.com>
2025-01-09 13:31:12 +0000

    added cards to the layout

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Alert from 'react-bootstrap/Alert';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Title1 from './Title1.js';
 
+const titleStyle = {
+	color: 'blue'
+}
+
 function App() {
   return (
     <div className="App">
@@ -15,20 +19,44 @@ function App() {
 	<div class="container">
 	  <div class="row">
 	    <div class="col">
-	      <p>Raj's Music App</p>
+	      <p style={titleStyle} >Raj's Music App</p>
 	    </div>
 	    <div class="col">
 	      <p>Listen</p>
 	    </div>
 	  </div>
 	  <div class="row">
-	    <div class="col">
-	      <p>Placeholder song list</p>
+	    <div class="col-3">
+	      <p>Placeholder about, contact</p>
 	    </div>
-	    <div class = "col">
+	    <div class = "col-7">
+	      <div class = "row">
+	        <div class = "col">
+	          <div class = "card">
+	            <div class = "card-header">Name of Song</div>
+	    	    <img	/>
+	  	    <div class = "card-body">
+	  	      <p class = "card-text">
+	  	         Description of song here
+	  	      </p>
+	  	    </div>
+	  	  </div>
+	        </div>
+                <div class = "col">
+	          <div class = "card">
+	            <div class = "card-header">Name of Song</div>
+	    	    <img	/>
+	  	    <div class = "card-body">
+	  	      <p class = "card-text">
+	  	         Description of song here
+	  	      </p>
+	  	    </div>
+	  	  </div>
+	        </div>
+	      </div>
 	      <p>Placeholder media library</p>
 	    </div>
-	    <div class = "col">
+	    <div class = "col-2">
 	      <p>Placeholder media player</p>
 	    </div>
 	  </div>
