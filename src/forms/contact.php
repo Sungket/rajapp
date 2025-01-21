3a9dad8b53cb2667a1c5252ea417cefb6992d3cd
sungket <93491754+Sungket@users.noreply.github.com>
2025-01-21 16:57:09 +0000

    added some code to contact.php

---
+++ src/forms/contact.php
@@ -0,0 +1,8 @@
+<?php
+
+   ini_set('display_error', 'On');
+      error_reporting(E_ALL);
+
+   $name = $_POST["inputName"];
+   $email = $_POST["inputEmail"];
+   $message = $_POST["inputMessage"];
